1b47ab05d7fb3710fff20854d7f388b4df58cca2
DoMo <ericdominguezm@gmail.com>
2023-09-02 04:58:05 +0200

    totally structure added, with tests and more

--- src/constants.py
+++ src/constants.py
@@ -8,7 +8,10 @@ PROJECT_ALREADY_EXISTS = "A directory with this project name already exists."
 INVALID_NAME_MODULE = "Invalid module name: {module}"
 
 # File paths
+SOURCE_PATH = "src"
+TESTS_PATH = "tests"
 TEMPLATES_MODULE = "src.templates"
+ROOT_STRUCTURE_JSON = "root_structure.json"
 BASE_STRUCTURE_JSON = "base_structure.json"
 MODULE_STRUCTURE_JSON = "module_structure.json"
 
@@ -17,3 +20,4 @@ MODULE_NAME_REPLACEMENT = "{module_name}"
 
 # Formats
 DESCRIPTION_FORMAT = '"""\n{value}\n"""'
+PREFIX_TEST_MODULE = "test_"
--- src/utils/structure.py
+++ src/utils/structure.py
@@ -3,11 +3,20 @@ This module contains the functions for creating the folder and file structure.
 """
 
 import os
+from copy import deepcopy
 
 from src.utils.structure_loader import load_structure_from_json
-from src.constants import MODULE_STRUCTURE_JSON, MODULE_NAME_REPLACEMENT, DESCRIPTION_FORMAT
+from src.constants import (
+    MODULE_STRUCTURE_JSON,
+    MODULE_NAME_REPLACEMENT,
+    DESCRIPTION_FORMAT,
+    SOURCE_PATH,
+    TESTS_PATH,
+    PREFIX_TEST_MODULE,
+)
 
 
+# TODO: migrate from recursion to iteration
 def create_structure(structure: dict, parent_folder: str = ""):
     """
     Creates the folder and file structure based on the given dictionary.
@@ -24,6 +33,7 @@ def create_structure(structure: dict, parent_folder: str = ""):
                 _f.write(description)
 
 
+# TODO: migrate from recursion to iteration
 def add_module_to_base_structure(base_structure: dict, module_structure: dict, module_name: str):
     """
     Add the module to the base structure.
@@ -40,10 +50,27 @@ def add_module_to_base_structure(base_structure: dict, module_structure: dict, m
             base_structure[formatted_key] = value
 
 
-def add_modules_to_base_structure(structure: dict, module_names: list[str]):
+# TODO: migrate from recursion to iteration
+def migrate_to_test_structure(structure: dict):
+    """
+    Migrate the structure to the test structure.
+    """
+    for key, value in structure.copy().items():
+        if isinstance(value, dict):
+            migrate_to_test_structure(value)
+        else:
+            structure[PREFIX_TEST_MODULE + key] = ""
+            del structure[key]
+
+    return structure
+
+
+def add_modules_to_root_structure(structure: dict, module_names: list[str]):
     """
     Add the module names to the structure.
     """
     module_structure = load_structure_from_json(MODULE_STRUCTURE_JSON)
+    test_module_structure = migrate_to_test_structure(deepcopy(module_structure))
     for module_name in module_names:
-        add_module_to_base_structure(structure, module_structure, module_name)
+        add_module_to_base_structure(structure[SOURCE_PATH], module_structure, module_name)
+        add_module_to_base_structure(structure[TESTS_PATH], test_module_structure, module_name)
